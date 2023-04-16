8e09a8c9ee247f5796afe1f63422f5a829d4777a
Ali <90877977+AlhakeemALI@users.noreply.github.com>
2023-04-16 16:26:22 -0600

    Create function to Calculate

--- Blackjack Capstone Project/main.py
+++ Blackjack Capstone Project/main.py
@@ -8,15 +8,37 @@ def deal_card():
    card = random.choice(cards)
    return card
 
+def calculate_score(list):
+    score = 0
+    for index in list:
+        score += index
+        if score == 21 and len(list) == 2:
+            return 0
+    if score > 21 and 11 in list:
+        print(list)
+        list.remove(11)
+        list.append(1)
+        # score = sum(list)
+        print(list)
+        print(score)
+    return score
+
 user_cards = []
 computer_cards = []
 
 for _ in range(2):
   user_cards.append(deal_card())
   computer_cards.append(deal_card())
-  print(user_cards, computer_cards)
+  # print(user_cards, computer_cards)
    
 
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(user_score, computer_score)
+
+    
+   
+  
 
 
 
